ff7e6b5f5cf952bb2acfe28f6ab1601a821b09c0
jumpegg <jumpegg@naver.com>
2016-09-18 18:46:15 +0900

    sql update

--- angular/merge/public/angular/board/board.js
+++ angular/merge/public/angular/board/board.js
@@ -9,4 +9,52 @@ angular.module('board',[
   $locationProvider.hashPrefix('!');
 
   $routeProvider.otherwise({redirectTo: '/'});
-}]);
+}])
+.controller('boardCtrl', function($scope){
+
+})
+.factory('boardService', function($http){
+	return{
+		getguest: function(callback){
+			$http.get('/board/getguest')
+			.success(function(data){
+				callback(data);
+			}).error(function(status){
+				console.log(status);
+			});
+		},
+		getfreetalk: function(callback){
+			$http.get('/board/getfreetalk')
+			.success(function(data){
+				callback(data);
+			}).error(function(status){
+				console.log(status);
+			});
+		},
+		getnotice: function(callback){
+			$http.get('/board/getnotice')
+			.success(function(data){
+				callback(data);
+			}).error(function(status){
+				console.log(status);
+			});
+		},
+		getschedule: function(callback){
+			$http.get('/board/getschedule')
+			.success(function(data){
+				callback(data);
+			}).error(function(status){
+				console.log(status);
+			});
+		},
+		getattendUser: function(callback){
+			$http.get('/board/getattendUser')
+			.success(function(data){
+				callback(data);
+			}).error(function(status){
+				console.log(status);
+			});
+		}
+
+	}
+});
--- angular/merge/router/board_router.js
+++ angular/merge/router/board_router.js
@@ -41,7 +41,7 @@ module.exports = function(app, mysqlClient, passport)
 			}
 		});
 	});
-	app.get('/board/attendUser', function(req, res){
+	app.get('/board/getattendUser', function(req, res){
 		mysqlClient.query('select * from attendUser where board_id = ?',[req.session.board_id], function(error, result){
 			if(error){
 				console.log('server error');
@@ -50,5 +50,183 @@ module.exports = function(app, mysqlClient, passport)
 			}
 		});
 	});
-
+	app.post('/board/newguest', function(req, res){
+		mysqlClient.query('insert into guest(board_id, user_id, admin_auth, nickname, join_date) values(?,?,false,?,now())', 
+			[req.session.board_id, req.session.index, req.body.nickname], 
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.post('/board/newfreetalk', function(req, res){
+		mysqlClient.query('insert into freetalk(board_id, user_id, title, content, cnt, create_date, available) values(?,?,?,?,0,now(), true)', 
+			[req.session.board_id, req.session.index, req.body.title, req.body.content], 
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.post('/board/newnotice', function(req, res){
+		mysqlClient.query('insert into notice(board_id, user_id, title, content, cnt, create_date, available) values(?,?,?,?,0,now(),true)',
+			[req.session.board_id, req.session.index, req.body.title, req.body.content],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.post('/board/newschedule', function(req, res){
+		mysqlClient.query('insert into schedule(board_id, user_id, title, place, gathering_time, t_cost, e_cost, cnt, create_date) values(?,?,?,?,?,?,?,0,now())',
+			[req.session.board_id, req.session.index, req.body.title, req.body.place, req.body.gathering_time, req.body.t_cost, req.body.e_cost],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/newattendUser/:s_id/:u_id', function(req, res){
+		mysqlClient.query('insert into attendUser(schedule_id, user_id) values(?,?)',
+			[req.params.s_id, req.params.u_id],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.post('/board/upguest', function(req, res){
+		mysqlClient.query('update guest set nickname = ? where id = ?',
+			[req.body.nickname, req.body.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.post('/board/upfreetalk', function(req, res){
+		mysqlClient.query('update freetalk set title = ?, content = ?, update_date = now() where id = ?',
+			[req.body.title, req.body.content, req.body.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.post('/board/upnotice', function(req, res){
+		mysqlClient.query('update notice set title = ?, content = ?, update_date = now() where id = ?',
+			[req.body.title, req.body.content, req.body.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.post('/board/upschedule', function(req, res){
+		mysqlClient.query('update schedule set title=?, place=?, gathering_time=?, t_cost=?, e_cost, update_date=now()',
+			[req.body.title, req.body.place, req.body.gathering_time, req.body.t_coast, req.body.e_cost],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/hidefreetalk/:index', function(req, res){
+		mysqlClient.query('update freetalk set available = false where id=?',[req.params.index], 
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/hidenotice/:index', function(req, res){
+		mysqlClient.query('update notice set available = false where id = ?', [req.params.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/hideschedule/:index', function(req, res){
+		mysqlClient.query('update schedule set available = false where id = ?', [req.params.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/delguest/:index', function(req, res){
+		mysqlClient.query('delete from guest where id = ?', [req.params.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/delfreetalk/:index', function(req, res){
+		mysqlClient.query('delete from freetalk where id = ?', [req.params.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/delnotice/:index', function(req, res){
+		mysqlClient.query('delete from notice where id = ?', [req.params.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/delschedule/:index', function(req, res){
+		mysqlClient.query('delete from schedule where id = ?', [req.params.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
+	app.get('/board/delattendUser/:index', function(req, res){
+		mysqlClient.query('delete from attendUser where id = ?', [req.params.index],
+			function(error, result){
+				if(error){
+					console.log('server error');
+				}else{
+					res.json({message : 'success'});
+				}
+			});
+	});
 }
